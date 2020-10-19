35e224ff8116012deb1dc888f65c3332df58bda5
Gabriel Brieva <3dmann@gmail.com>
2020-10-19 02:40:35 -0300

    remove TODO comments

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -8,10 +8,6 @@ import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
-
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 const jwksUrl = 'https://dev-saet8-p4.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
@@ -56,10 +52,6 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
 
   try {
     const jwt: Jwt = decode(token, { complete: true }) as Jwt
